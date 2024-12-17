c09c8ac4f84a92abe17f69a512a814536ebaa768
Geoff Fletcher <xov4m05a@students.codeinstitute.net>
2024-12-17 10:21:37 +0000

    link cloudinary, mod static files settings

--- true_review/settings.py
+++ true_review/settings.py
@@ -22,6 +22,7 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR
 
 
 # Quick-start development settings - unsuitable for production
@@ -47,6 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'review',
 ]
 
@@ -132,6 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
