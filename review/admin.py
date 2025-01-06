c3e69ec9386d1d8fe680a7fd80e71cd80b0af722
Geoff Fletcher <xov4m05a@students.codeinstitute.net>
2025-01-06 16:46:46 +0000

    add admin changes to allow creation of reviews on the backend to test views

--- review/admin.py
+++ review/admin.py
@@ -1,14 +1,56 @@
 from django.contrib import admin
-from .models import Category
-from .models import CommonReviewData
-from .models import TVReview
-from .models import MovieReview
-from .models import GameReview
+from .models import Category, MovieReview, TVReview, GameReview
 
-admin.site.register(Category)
-admin.site.register(CommonReviewData)
-admin.site.register(TVReview)
-admin.site.register(MovieReview)
-admin.site.register(GameReview)
+# Base class for common review fields
+class CommonReviewAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "rating", "status", "created_on")
+    search_fields = ("title", "author__username", "slug")
+    list_filter = ("status", "created_on", "release_date")
+    ordering = ["-created_on"]
+    readonly_fields = ("created_on",)  # Make created_on read-only
 
-# Register your models here.
+# MovieReview admin
+@admin.register(MovieReview)
+class MovieReviewAdmin(CommonReviewAdmin):
+    fieldsets = (
+        (None, {
+            'fields': (
+                'author', 'featured_image', 'title', 'slug', 'review_text',
+                'release_date', 'rating', 'status', 'created_on',  # Read-only
+                'runtime', 'director', 'category'
+            )
+        }),
+    )
+
+# TVReview admin
+@admin.register(TVReview)
+class TVReviewAdmin(CommonReviewAdmin):
+    fieldsets = (
+        (None, {
+            'fields': (
+                'author', 'featured_image', 'title', 'slug', 'review_text',
+                'release_date', 'rating', 'status', 'created_on',  # Read-only
+                'number_of_episodes', 'showrunner', 'average_episode_length', 
+                'category'
+            )
+        }),
+    )
+
+# GameReview admin
+@admin.register(GameReview)
+class GameReviewAdmin(CommonReviewAdmin):
+    fieldsets = (
+        (None, {
+            'fields': (
+                'author', 'featured_image', 'title', 'slug', 'review_text',
+                'release_date', 'rating', 'status', 'created_on',  # Read-only
+                'studio', 'time_to_beat', 'number_of_players', 'category'
+            )
+        }),
+    )
+
+# Register Category separately
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
